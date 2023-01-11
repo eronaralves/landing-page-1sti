dc1098ac7459d4a53d55039dadbda8e9e069f191
eronaralves <alveseronar@gmail.com>
2023-01-11 10:38:13 -0300

    config project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from '../styles/global'
+
+export default function Home() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>
+        hello world
+      </h1>
+    </div>
+  )
+}
